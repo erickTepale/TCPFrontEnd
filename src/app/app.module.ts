37676c128f40ab0c6c27950e392ed34a6ffeb001
Erick Tepale <e.tepale@gmail.com>
2019-07-22 17:25:13 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ChatpageComponent } from './component/chatpage/chatpage.component';
+import { ListComponent } from './component/list/list.component';
+import { AboutComponent } from './component/about/about.component';
+import { NavComponent } from './component/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChatpageComponent,
+    ListComponent,
+    AboutComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
