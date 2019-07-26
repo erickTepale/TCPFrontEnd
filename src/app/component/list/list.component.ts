45b39c42f2160b5642ecd5ca392ceaf5103715aa
prasanthipenmadu <prasanthipl@gmail.com>
2019-07-26 17:04:42 -0400

    add channel and add admin to the channel

--- src/app/component/list/list.component.ts
+++ src/app/component/list/list.component.ts
@@ -9,6 +9,7 @@ import { DirectMessageService } from 'src/app/services/chat.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/classes/User';
 import { CurrentUser } from 'src/app/classes/CurrentUser';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-list',
@@ -18,8 +19,13 @@ import { CurrentUser } from 'src/app/classes/CurrentUser';
 export class ListComponent implements OnInit {
   channels:Channel[];
   users: CurrentUser[];
-
+  channel:Channel;
+  channelBody:Channel;
+  add: Boolean = false;
+  cname:string;
+  cpublics:boolean;
   constructor(
+    private loginService:LoginService,
     private cmService: CmListService,
     private dmService: DmListService,
     private navService: NavService,
@@ -52,6 +58,14 @@ export class ListComponent implements OnInit {
       }
     );
     this.channels = null;
+  }
+
+  onClick(event: any) {
+    this.add=true;
+    console.log(this.add);
+  }
+  onClick1(event:any){
+this.cmService.postChannel(this.loginService.currentUser.user_id,this.cname,this.cpublics);
   }
 
   channelClick(channel:Channel){
